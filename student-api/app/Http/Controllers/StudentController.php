1b7d88bd1760815dcee541a83d2a77b48a586cf4
JOHNBSE <johnwyc256@gmail.com>
2026-03-04 10:58:08 -0800

    student-api initiaal commit

---
+++ student-api/app/Http/Controllers/StudentController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Student::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {   
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:students',
+            'course' => 'required|string',
+        ]);
+
+        $student = Student::create($validated);
+
+        return response()->json($student, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Student $student)
+    {
+        return response()->json($student);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Student $student)
+    {
+         $validated = $request->validate([
+            'name' => 'sometimes|required|string',
+            'email' => 'sometimes|required|string|email|unique:students,email,' . $student->id,
+            'course' => 'sometimes|required|string',
+        ]);
+
+        $student->update($validated);
+
+        return response()->json($student, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Student $student)
+    {
+        $student->delete();
+
+        return response()->json(null, 204);
+    }
+}
