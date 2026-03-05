cd3da4a52de0ef276dcff46d7aa709a10b295aa7
JOHNBSE <johnwyc256@gmail.com>
2026-03-05 05:01:26 -0800

    Added validtion rules that apply to requests

--- student-api/app/Http/Requests/StoreStudentRequest.php
+++ student-api/app/Http/Requests/StoreStudentRequest.php
@@ -22,7 +22,9 @@ class StoreStudentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:students,email',
+            'course' => 'required|string|max:255',
         ];
     }
 }
